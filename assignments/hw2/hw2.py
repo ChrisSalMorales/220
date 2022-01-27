f9194f1ca8c8e673169cd72813856add5d9f42c3
chrismorales <moralescs@g.cofc.edu>
2022-01-27 16:22:10 -0500

    Completed Homework 2

--- assignments/hw2/hw2.py
+++ assignments/hw2/hw2.py
@@ -1,36 +1,56 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Christopher Morales
+homework_2.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: The task given is to provide programs that use only inputs, outputs, and arithmetics.
 
 Certification of Authenticity:
-<include one of the following>
-I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
+I certify that this assignment is my own work, but I discussed it with:
+Brooke Duvall in the Computer Science Tutoring Lab
 """
 import math
 
 
 def sum_of_threes():
-    pass
+    sum_of_three = 0
+    upper_bound = eval(input("What is the upper bond?"))
+    for i in range(0, upper_bound + 1):
+        if i % 3 == 0:
+            sum_of_three += i
+    print("sum of threes is", sum_of_three)
 
 
 def multiplication_table():
-    pass
+
+    for multiplication_one in range(1, 11):
+        for multiplication_two in range(1, 11):
+            print(multiplication_one * multiplication_two, end="\t")
+        print()
 
 
 def triangle_area():
-    pass
+    a_length = eval(input("Enter side a length: "))
+    b_length = eval(input("Enter side b length: "))
+    c_length = eval(input("Enter side c length: "))
+    s = (a_length + b_length + c_length) / 2
+    before_squaring = s * (s - a_length) * (s - b_length) * (s - c_length)
+    area_of_a_triangle = math.sqrt(before_squaring)
+    print("area is", area_of_a_triangle)
 
 
 def sum_squares():
-    pass
+    sum_squares_lower_and_upper_range = 0
+    lower_range = eval(input("Enter lower range:"))
+    upper_range = eval(input("Enter upper range:"))
+    for sum_times_squares in range(lower_range, upper_range + 1):
+        sum_squares_lower_and_upper_range += sum_times_squares * sum_times_squares
+    print(sum_squares_lower_and_upper_range)
 
 
 def power():
-    pass
-
-
-if __name__ == '__main__':
-    pass
+    power_result = 1
+    base = eval(input("Enter base:"))
+    exponent = eval(input("Enter exponent:"))
+    for power_loop in range(1, exponent + 1):
+        power_result = base * power_result
+    print(base, "^", exponent, "=", power_result)
--- notes/Practice.py
+++ notes/Practice.py
@@ -1,9 +1,73 @@
-def future():
-    age = eval(input("enter your age:"))
-    futureage = age + 20
-    print("in 20 years you will be", futureage)
+#def future():
+    #age = eval(input("enter your age:"))
+    #futureage = age + 20
+    #print("in 20 years you will be", futureage)
 
-def convert():
-    celsius = eval(input("enter temp in celsius:"))
-    fahrenheit = celsius * 9 / 5 + 32
-    print("that is", fahrenheit,"degrees fahrenheit")
+
+#def convert():
+    # get user input in celsius (This is a comment)
+    #celsius = eval(input("enter temp in celsius:"))
+    #fahrenheit = celsius * 9 / 5 + 32
+
+#display result to user
+#print("that is", fahrenheit, "degrees fahrenheit")
+
+
+#print("hello", end="")
+
+
+#print("world")
+
+
+#name, age = eval(input("hello!\nEnter your name and age: "))
+#print("good to see you", name),"you are", age, "years old!")
+
+
+#hour, minute = eval(input("Enter the hour and minute: "))
+#print("it is", hour, ":", minute)
+
+
+#for element in range(10):
+#print(element, end=" ")
+
+
+#principal =eval(input("enter principal balance: "))
+#apr = eval(input('enter interest rate: '))
+#for i in range(10):
+#   principal = principal * (1+apr)
+#  print(principal)
+#print("the final balance is ", principal)
+
+# for i in range(5):
+#    for j in range(5):
+#         print(i, j, end='-')
+#     print()
+
+import math
+
+# dot notation
+# x = math.sqrt(9)
+# print(x)
+# x = math.pow(2, 3) # parameters / arguments
+# print(x)
+# x = 2.0 ** 3
+# print(x)
+
+
+# my_range = range(10) # stop - default start 0, default step 1
+# my_range = range(3, 10) # (start, stop) default step 1
+# my_range = range(3, 10, 2) # (start, stop, step)
+# my_range = range(10, 3, -1)
+# print(list(my_range))
+
+# accumulator pattern
+# my_sum = 0
+# for i in range(101):
+#     my_sum = my_sum + i
+# print(my_sum)
+
+user_input = eval(input("Enter a number: "))
+fact = 1
+for i in range(user_input, 0, -1):
+    fact =  fact * i
+print(fact)
